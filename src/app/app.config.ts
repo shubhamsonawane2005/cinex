80bd2d5c16f3289e133be1a7ed35272217d91411
Piyush <piyush@gmail.com>
2026-02-15 19:21:59 +0530

    Upadte in email login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -10,13 +10,13 @@
 // };
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // 1. Ye line add kar
+import { provideHttpClient, withFetch } from '@angular/common/http'; // 1. withFetch yahan add kar
 
 import { routes } from './app.routes'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient() // 2. Ye line yahan add kar
+    provideHttpClient(withFetch()) // 2. Yahan bracket ke andar withFetch() daal de
   ]
 };
