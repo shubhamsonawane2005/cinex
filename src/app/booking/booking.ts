929718c49c1608539a37dbc325c9e8554bb05498
Piyush <piyush@gmail.com>
2026-02-19 15:14:29 +0530

    Added showtime selection in booking and displayed the selected time on the payment page

--- src/app/booking/booking.ts
+++ src/app/booking/booking.ts
@@ -1,105 +1,4 @@
-// import { Component, OnInit, inject } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { ActivatedRoute, RouterModule, Router } from '@angular/router'; 
-// import { FormsModule } from '@angular/forms'; 
-// import { MovieService } from '../services/movie'; 
 
-// @Component({
-//   selector: 'app-booking',
-//   standalone: true,
-//   imports: [CommonModule, RouterModule, FormsModule], 
-//   templateUrl: './booking.html',
-//   styleUrl: './booking.css'
-// })
-// export class BookingComponent implements OnInit {
-  
-//   movieTitle: string = ""; 
-//   theaterName: string = ""; 
-//   selectedSeats: string[] = [];
-//   totalPrice: number = 0;
-//   ticketPrice: number = 200; 
-
-//   seatsToBook: number = 1; 
-//   bookedSeats: string[] = ['A2', 'C5', 'E1']; 
-
-//   times: string[] = ['10:00 PM', '01:00 PM']; 
-//   selectedTime: string = '10:00 PM';
-//   rows: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
-//   seatsPerRow: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  
-//   private route = inject(ActivatedRoute);
-//   private movieService = inject(MovieService);
-//   private router = inject(Router);
-
-//   ngOnInit() {
-//     this.route.queryParams.subscribe(params => {
-//       console.log('Received Params:', params); 
-
-//       if (params['name']) {
-//         this.movieTitle = params['name'];
-//       }
-      
-//       // Home page se jo theater aayega wahi save hoga
-//       if (params['theater']) {
-//         this.theaterName = params['theater'];
-//       } else if (!this.theaterName) {
-//         this.theaterName = "PVR: Rahul Raj Mall"; 
-//       }
-
-//       if (params['time']) {
-//         this.selectedTime = params['time'];
-//       }
-
-//       const idFromPath = this.route.snapshot.paramMap.get('id') || this.route.snapshot.paramMap.get('movieId');
-//       if (idFromPath && !this.movieTitle) {
-//         this.movieService.getMovieById(Number(idFromPath)).subscribe(movie => {
-//           if (movie) {
-//             this.movieTitle = movie.title;
-//           }
-//         });
-//       }
-//     });
-//   }
-
-//   // YE FUNCTION AB LATEST DATA BHEJEGA
-//   goToPayment() {
-//     this.router.navigate(['/payment'], {
-//       queryParams: {
-//         movie: this.movieTitle,
-//         price: this.totalPrice,
-//         seats: this.selectedSeats.join(', '),
-//         time: this.selectedTime,   // Latest selected time
-//         theater: this.theaterName  // Latest theater name
-//       }
-//     });
-//   }
-
-//   selectTime(time: string) {
-//     this.selectedTime = time; // Time update karne ke liye
-//   }
-
-//   toggleSeat(row: string, seatNum: number) {
-//     this.selectedSeats = [];
-//     for (let i = 0; i < this.seatsToBook; i++) {
-//       const currentSeatNum = seatNum + i;
-//       const seatId = row + currentSeatNum;
-//       if (currentSeatNum <= 9 && !this.isBooked(row, currentSeatNum)) {
-//         this.selectedSeats.push(seatId);
-//       } else {
-//         break; 
-//       }
-//     }
-//     this.totalPrice = this.selectedSeats.length * this.ticketPrice;
-//   }
-
-//   isSelected(row: string, seatNum: number): boolean {
-//     return this.selectedSeats.includes(row + seatNum);
-//   }
-
-//   isBooked(row: string, seatNum: number): boolean {
-//     return this.bookedSeats.includes(row + seatNum);
-//   }
-// }
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule, Router } from '@angular/router'; 
@@ -116,7 +15,7 @@ import { MovieService } from '../services/movie';
 export class BookingComponent implements OnInit {
   
   movieTitle: string = ""; 
-  theaterName: string = ""; // Default Khali rakho
+  theaterName: string = ""; 
   selectedSeats: string[] = [];
   totalPrice: number = 0;
   ticketPrice: number = 200; 
@@ -124,8 +23,10 @@ export class BookingComponent implements OnInit {
   seatsToBook: number = 1; 
   bookedSeats: string[] = ['A2', 'C5', 'E1']; 
 
-  times: string[] = ['10:00 PM', '01:00 PM']; 
-  selectedTime: string = '10:00 PM';
+  // Inko static rakha hai, par UI mein selection URL wale se match hoga
+  times: string[] = ['10:00 AM', '11:00 AM', '01:00 PM', '01:30 PM', '01:45 PM', '02:00 PM', '04:30 PM', '05:00 PM', '05:30 PM', '06:15 PM', '08:15 PM', '09:00 PM', '09:30 AM', '09:45 PM', '10:15 AM', '10:30 PM', '12:45 PM']; 
+  selectedTime: string = ""; // Shuru mein khali taaki URL wala value override kare
+  
   rows: string[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
   seatsPerRow: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   
@@ -134,7 +35,7 @@ export class BookingComponent implements OnInit {
   private router = inject(Router);
 
   ngOnInit() {
-    // SIRF EK BAR DATA FETCH KARO TAAKI OVERWRITE NA HO
+    // Path Params se data uthana (movieId, theaterId, time)
     this.route.paramMap.subscribe(params => {
       const movieId = params.get('movieId');
       const theaterId = params.get('theaterId');
@@ -147,19 +48,25 @@ export class BookingComponent implements OnInit {
         '4': 'Rajhans Multiplex'
       };
 
-      // ID se naam uthao, agar ID nahi hai toh QueryParams se uthao
+      // 1. Theater Name set karo ID ke basis par
       if (theaterId && theaterMap[theaterId]) {
         this.theaterName = theaterMap[theaterId];
       } else {
-        // QueryParams backup
         const qTheater = this.route.snapshot.queryParams['theater'];
         this.theaterName = qTheater || "PVR: Rahul Raj Mall"; 
       }
 
+      // 2. TIME SELECTION FIX: Jo pichle page se select hoke aaya wahi yahan dikhega
       if (timeFromPath) {
-        this.selectedTime = timeFromPath;
+        this.selectedTime = decodeURIComponent(timeFromPath);
+        
+        // Agar ye time hamari list mein nahi hai, toh use list mein add kar do taaki button dikhe
+        if (!this.times.includes(this.selectedTime)) {
+          this.times.push(this.selectedTime);
+        }
       }
 
+      // 3. Movie details fetch karo
       if (movieId) {
         this.movieService.getMovieById(Number(movieId)).subscribe(movie => {
           if (movie) this.movieTitle = movie.title;
@@ -169,9 +76,7 @@ export class BookingComponent implements OnInit {
   }
 
   goToPayment() {
-    // Navigation se pehle check karo data sahi hai
-    console.log("Navigating with Theater:", this.theaterName);
-    
+    // Payment page par data transfer
     this.router.navigate(['/payment'], {
       queryParams: {
         movie: this.movieTitle,
--- src/app/movie-details/movie-details.ts
+++ src/app/movie-details/movie-details.ts
@@ -1,186 +1,3 @@
-// import { Component, OnInit, inject } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { ActivatedRoute, RouterModule, Router } from '@angular/router'; // Added Router
-// import { MovieService, Movie } from '../services/movie';
-
-// @Component({
-//   selector: 'app-movie-details',
-//   standalone: true,
-//   imports: [CommonModule, RouterModule],
-//   templateUrl: './movie-details.html',
-//   styleUrl: './movie-details.css'
-// })
-// export class MovieDetailsComponent implements OnInit {
-
-//   movie: Movie | undefined;
-//   selectedTheater: number | null = null;
-//   selectedTime: string | null = null;
-
-//   private route = inject(ActivatedRoute);
-//   private router = inject(Router); // Inject Router for navigation
-//   private movieService = inject(MovieService);
-//   dates: any[] = [];
-//   similarMovies: Movie[] = [];
-
-//   cast = [
-//     { name: 'Cillian Murphy', role: 'Oppenheimer', image: 'https://upload.wikimedia.org/wikipedia/commons/a/a5/Cillian_Murphy_Press_Conference_The_Party_Berlinale_2017_02cr.jpg' },
-//     { name: 'Robert Downey Jr.', role: 'Lewis Strauss', image: 'https://upload.wikimedia.org/wikipedia/commons/9/94/Robert_Downey_Jr_2014_Comic_Con_%28cropped%29.jpg' },
-//     { name: 'Emily Blunt', role: 'Kitty', image: 'https://upload.wikimedia.org/wikipedia/commons/3/3b/Emily_Blunt_by_Gage_Skidmore_2.jpg' },
-//     { name: 'Matt Damon', role: 'Leslie Groves', image: 'https://upload.wikimedia.org/wikipedia/commons/8/82/Damon_Matt_2009_Venice_Film_Festival.jpg' },
-//     { name: 'Florence Pugh', role: 'Jean Tatlock', image: 'https://upload.wikimedia.org/wikipedia/commons/6/68/Florence_Pugh_SDCC_2019.jpg' }
-//   ];
-
-
-//   ngOnInit() {
-//     // 1. Generate live dates first
-//     this.generateLiveDates();
-
-//     // 2. Load movie data
-//     this.route.paramMap.subscribe(params => {
-//       const id = Number(params.get('id'));
-//       if (id) {
-//         this.loadMovie(id);
-//       }
-//     });
-//   }
-//   generateLiveDates() {
-//     const dayNames = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-//     const monthNames = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
-
-//     const tempDates = [];
-
-//     for (let i = 0; i < 7; i++) {
-//       const d = new Date();
-//       d.setDate(d.getDate() + i);
-
-//       let weekdayLabel = dayNames[d.getDay()];
-//       if (i === 0) weekdayLabel = 'TODAY';
-//       if (i === 1) weekdayLabel = 'TOMORROW';
-
-//       tempDates.push({
-//         day: d.getDate().toString().padStart(2, '0'),
-//         month: monthNames[d.getMonth()],
-//         weekday: weekdayLabel,
-//         active: i === 0,
-//         fullDate: d
-//       });
-//     }
-//     this.dates = tempDates;
-//   }
-
-//   // Booking time 
-//   isTimePassed(timeStr: string): boolean {
-    
-//   // 1. Find the currently active date card
-//   const activeDate = this.dates.find(d => d.active);
-  
-//   // 2. If the selected date is NOT "TODAY", all shows are available
-//   if (!activeDate || activeDate.weekday !== 'TODAY') {
-//     return false;
-//   }
-
-//   // 3. Convert time string (e.g., "01:30 PM") to a 24-hour Date object
-//   const now = new Date();
-//   const [time, modifier] = timeStr.split(' ');
-//   let [hours, minutes] = time.split(':').map(Number);
-
-//   if (modifier === 'PM' && hours < 12) hours += 12;
-//   if (modifier === 'AM' && hours === 12) hours = 0;
-
-//   const showTime = new Date();
-//   showTime.setHours(hours, minutes, 0, 0);
-
-//   // 4. Compare: If showTime is before now, it's passed
-//   return showTime < now;
-// }
-
-
-//   // Theaters Data (Integrated from previous component)
-//   theaters = [
-//     {
-//       id: 1,
-//       name: 'PVR: Rahul Raj Mall',
-//       address: 'Piplod, Dumas Road, Surat',
-//       timings: ['10:00 AM', '01:30 PM', '05:00 PM', '09:00 PM'],
-//       mapUrl: 'https://www.google.com/maps/search/?api=1&query=PVR+Rahul+Raj+Mall+Surat'
-//     },
-//     {
-//       id: 2,
-//       name: 'INOX: VR Mall',
-//       address: 'Dumas Road, Magdalla, Surat',
-//       timings: ['11:00 AM', '02:00 PM', '06:15 PM', '10:30 PM'],
-//       mapUrl: 'https://www.google.com/maps/search/?api=1&query=INOX+VR+Mall+Surat'
-//     },
-//     {
-//       id: 3,
-//       name: 'Cinépolis: Imperial Square',
-//       address: 'Adajan Gam, Surat',
-//       timings: ['09:30 AM', '12:45 PM', '04:30 PM', '08:15 PM'],
-//       mapUrl: 'https://www.google.com/maps/search/?api=1&query=Cinepolis+Imperial+Square+Mall+Surat'
-//     },
-//     {
-//       id: 4,
-//       name: 'Rajhans Multiplex',
-//       address: 'Pal Hazira Road, Adajan, Surat',
-//       timings: ['10:15 AM', '01:45 PM', '05:30 PM', '09:45 PM'],
-//       mapUrl: 'https://www.google.com/maps/search/?api=1&query=Rajhans+Cinema+Pal+Adajan+Surat'
-//     }
-//   ];
-
-
-//   loadMovie(id: number) {
-//     this.movieService.getMovieById(id).subscribe(data => {
-//       this.movie = data;
-//     });
-
-//     this.movieService.getMovies().subscribe(data => {
-//       this.similarMovies = data.filter(m => m.id !== id);
-//     });
-//   }
-
-//   selectDate(index: number) {
-//     this.dates.forEach(d => d.active = false);
-//     this.dates[index].active = true;
-//     this.selectedTime = null;
-//     this.selectedTheater = null;
-//   }
-
-//   selectTime(time: string, theaterId: number) {
-//     this.selectedTime = time;
-//     this.selectedTheater = theaterId;
-//     console.log(`Booking ${this.movie?.title} at Theater ${theaterId} for ${time}`);
-//   }
-
-
-//   proceedToBooking() {
-//     if (this.movie && this.selectedTheater && this.selectedTime) {
-
-//       // YAHAN FIX HAI: ID se theater ka NAME nikalna padega navigation ke liye
-//       const selectedTheaterObj = this.theaters.find(t => t.id === this.selectedTheater);
-//       const theaterName = selectedTheaterObj ? selectedTheaterObj.name : '';
-
-//       // Navigate with QueryParams so the Booking page can catch 'theater' and 'time'
-//       this.router.navigate(['/booking', this.movie.id], {
-//         queryParams: {
-//           name: this.movie.title,
-//           theater: theaterName,
-//           time: this.selectedTime
-//         }
-//       });
-
-//     } else {
-//       alert('Please select a theater and a showtime first.');
-//       this.scrollToBooking();
-//     }
-//   }
-  
-//   scrollToBooking() {
-//     const element = document.getElementById('booking-section');
-//     if (element) {
-//       element.scrollIntoView({ behavior: 'smooth', block: 'start' });
-//     }
-//   }
-// }
 import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute, RouterModule, Router } from '@angular/router';
@@ -277,15 +94,14 @@ export class MovieDetailsComponent implements OnInit {
   selectTime(time: string, theaterId: any) {
     this.selectedTime = time;
     this.selectedTheater = Number(theaterId);
-    console.log("Selected:", time, "Theater ID:", this.selectedTheater);
+    console.log("Time Selected:", time, "Theater ID:", this.selectedTheater);
   }
 
-  // --- YE WALA FUNCTION TERE ROUTES KE HISAB SE SAHI KIYA HAI ---
   proceedToBooking() {
     if (this.movie && this.selectedTheater && this.selectedTime) {
       
-      // Tere routes.ts mein rasta hai: booking/:movieId/:theaterId/:time
-      // Isliye humein navigate aise karna padega:
+      // Subham ki problem yahan solve hogi:
+      // Hum URL mein MovieId, TheaterId aur SelectedTime teeno bhej rahe hain
       this.router.navigate([
         '/booking', 
         this.movie.id, 
@@ -293,7 +109,7 @@ export class MovieDetailsComponent implements OnInit {
         this.selectedTime
       ]);
 
-      console.log("Navigating to booking page...");
+      console.log("Navigating with:", this.movie.id, this.selectedTheater, this.selectedTime);
 
     } else {
       alert('Please select a theater and a showtime first.');
